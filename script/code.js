368110bc9c181af50b291e74e02b82374ac36a25
D-Raff <damonraffels@gmail.com>
2023-11-24 11:16:15 +0200

    completed js

--- script/code.js
+++ script/code.js
@@ -1,1 +1,15 @@
 // function
+
+const btnCalc = document.querySelector("[data-calc]");
+
+function Calculate() {
+  let input = +document.querySelector("[data-mins]").value;
+
+  let output = document.querySelector("[data-res]");
+
+  let res = input * 60;
+  console.log(res);
+  output.textContent = `that is ${res} seconds!`;
+}
+
+btnCalc.addEventListener("click", Calculate);
